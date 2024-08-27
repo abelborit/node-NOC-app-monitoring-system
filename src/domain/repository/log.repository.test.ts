6d5f4c27744f7ddf16feaa9cb940e2b38f8ae5cb
Abel Borit <abelboritguitton@gmail.com>
2024-08-27 17:54:04 -0500

    [add] Test in log.repository.ts

---
+++ src/domain/repository/log.repository.test.ts
@@ -0,0 +1,5 @@
+describe("Test in log.repository.ts", () => {
+  test("should ", () => {
+    /* aquí sería hacer casi lo mismo que se hizo para el log.datasource.test.ts ya que su implementación es muy similar */
+  });
+});
